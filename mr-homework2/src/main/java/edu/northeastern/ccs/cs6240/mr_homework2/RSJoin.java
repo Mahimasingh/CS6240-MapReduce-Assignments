7eccd580923b304110ba87c1112573bb58fff605
Mahima Singh <mahimasingh@Mahimas-MacBook-Pro.local>
2018-10-03 20:44:52 -0400

    Completed RS Join but output looks funny

--- mr-homework2/src/main/java/edu/northeastern/ccs/cs6240/mr_homework2/RSJoin.java
+++ mr-homework2/src/main/java/edu/northeastern/ccs/cs6240/mr_homework2/RSJoin.java
@@ -1,29 +1,42 @@
 package edu.northeastern.ccs.cs6240.mr_homework2;
 
+import java.io.DataInput;
+import java.io.DataOutput;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.apache.commons.lang.StringUtils;
+import org.apache.hadoop.conf.Configuration;
 import org.apache.hadoop.conf.Configured;
+import org.apache.hadoop.fs.Path;
 import org.apache.hadoop.io.IntWritable;
 import org.apache.hadoop.io.Text;
+import org.apache.hadoop.io.Writable;
+import org.apache.hadoop.mapreduce.Job;
 import org.apache.hadoop.mapreduce.Mapper;
 import org.apache.hadoop.mapreduce.Reducer;
 import org.apache.hadoop.mapreduce.Mapper.Context;
+import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
+import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
 import org.apache.hadoop.util.Tool;
+import org.apache.hadoop.util.ToolRunner;
 import org.apache.log4j.LogManager;
 import org.apache.log4j.Logger;
 
-class FlaggedEdges{
+class FlaggedEdges implements Writable{
 	String from;
 	String to;
-	char flag;
-	public FlaggedEdges(String from, String to, char flag){
+	String flag;
+	public FlaggedEdges(String from, String to, String flag){
 		this.from = from;
 		this.to = to;
 		this.flag = flag;
 		}
-	public char getFlag() {
+	public FlaggedEdges() {
+		// TODO Auto-generated constructor stub
+	}
+	public String getFlag() {
 		return this.flag;
 	}
 	public String getFromNode() {
@@ -32,18 +45,23 @@ class FlaggedEdges{
 	public String getToNode() {
 		return this.to;
 	}
-}
-
-class Triangle{
-	String node1;
-	String node2;
-	String node3;
-	public Triangle(String n1,String n2,String n3) {
-		this.node1=n1;
-		this.node2=n2;
-		this.node3=n3;
+	@Override
+	public void write(DataOutput out) throws IOException {
+		out.writeChars(from);
+		out.writeChars(to);
+		out.writeChars(flag);
+		
+	}
+	@Override
+	public void readFields(DataInput in) throws IOException {
+		String line = in.readLine();
+		from = line.split(",")[0];
+	    to = line.split(",")[1];
+	    flag = Character.toString(line.split(",")[2].charAt(1));
+		
 	}
 }
+
 class Edge{
 	String from;
 	String to;
@@ -51,6 +69,16 @@ class Edge{
 		this.from = from;
 		this.to= to;
 	}
+	public Edge() {
+		// TODO Auto-generated constructor stub
+	}
+	public String getFrom() {
+		return this.from;
+	}
+	public String getTo() {
+		return this.to;
+	}
+	
 	
 }
 
@@ -60,36 +88,57 @@ public class RSJoin extends Configured implements Tool{
 	public static class RSMapper extends Mapper<Object, Text, Text, FlaggedEdges> {
 		private final Text from = new Text();
 		private final Text to = new Text();
-		private final Text val = new Text();
+		FlaggedEdges from_val = new FlaggedEdges();
+		FlaggedEdges to_val = new FlaggedEdges();
+		
 		@Override
 		public void map(final Object key, final Text value, final Context context) throws IOException, InterruptedException {
+			logger.info("ALL IS FINE mapper");
 			String line = value.toString();
 		    String[] user_follower = line.split(",");
 		    	from.set(user_follower[0]);
 		    	to.set(user_follower[1]);
-		    	FlaggedEdges from_val = new FlaggedEdges(user_follower[0],user_follower[1],'S');
-		    	FlaggedEdges to_val = new FlaggedEdges(user_follower[0],user_follower[1],'T');
+		    	from_val = new FlaggedEdges(user_follower[0]+",",user_follower[1]+",","S");
+		    	to_val = new FlaggedEdges(user_follower[0]+",",user_follower[1]+",","T");
 		    	context.write(from,from_val);
 		    	context.write(to, to_val);
 		    	
 		    }
 	}
-	public static class RSReducer extends Reducer<Text, FlaggedEdges, Text, Triangle> {
-		private final IntWritable result = new IntWritable();
+	public static class RSReducer extends Reducer<Text, FlaggedEdges, Text, Text> {
+		private Text from = new Text();
+		private Text to = new Text();
+		Edge e1 = new Edge();
+		Edge e2 = new Edge();
 
 		@Override
 		public void reduce(final Text key, final Iterable<FlaggedEdges> values, final Context context) throws IOException, InterruptedException {
+		
 			List<Edge> SList = new ArrayList<>();
 			List<Edge> TList = new ArrayList<>();
-			
-			for (final FlaggedEdges val : values) {
-				if(val.getFlag()=='S') {
-					Edge e = new Edge(val.getFromNode(),val.getToNode());
-					SList.add(e);
+			for (FlaggedEdges val : values) {
+				String flag = val.getFlag();
+				System.out.println("AFTER STORING FLAG" + flag);
+				if(flag.compareTo("T") == 0) {
+					e1 = new Edge(val.getFromNode(),val.getToNode());
+					SList.add(e1);
 				}
-				if(val.getFlag()=='T') {
-					Edge e = new Edge(val.getFromNode(),val.getToNode());
-					TList.add(e);
+				
+				else if(flag.compareTo("S") == 0) {
+					e2 = new Edge(val.getFromNode(),val.getToNode());
+					TList.add(e2);
+				}
+				else
+					logger.info("Something fishy happened!!" + flag.compareTo("S"));
+			}
+			
+			for(int i =0; i< SList.size();i++) {
+				for(int j = 0; j < TList.size(); j++) {
+					
+					from.set(SList.get(i).getFrom()+","+SList.get(i).getTo());
+					to.set(TList.get(j).getFrom()+","+TList.get(j).getTo());
+					logger.info("ALL IS FINE reducer3");
+					context.write(from,to);
 				}
 			}
 			
@@ -99,8 +148,33 @@ public class RSJoin extends Configured implements Tool{
 
 
 	public int run(String[] args) throws Exception {
-		// TODO Auto-generated method stub
-		return 0;
+		final Configuration conf = getConf();
+	    final Job job = Job.getInstance(conf, "Two Paths");
+		job.setJarByClass(RSJoin.class);
+		final Configuration jobConf = job.getConfiguration();
+		job.setMapperClass(RSMapper.class);
+		job.setReducerClass(RSReducer.class);
+		job.setOutputKeyClass(Text.class);
+		job.setOutputValueClass(Text.class);
+		job.setMapOutputKeyClass(Text.class);
+		job.setMapOutputValueClass(FlaggedEdges.class);
+		FileInputFormat.addInputPath(job, new Path(args[0]));
+		FileOutputFormat.setOutputPath(job, new Path(args[1]));
+		return job.waitForCompletion(true)?0:1;
+		
 	}
+	public static void main(final String[] args) {
+		if (args.length != 2) {
+			throw new Error("Two arguments required:\n<input-dir> <output-dir>");
+		}
 
+		try {
+			ToolRunner.run(new RSJoin(), args);
+		} catch (final Exception e) {
+			logger.error("", e);
+		}
+	}
+		
 }
+
+
